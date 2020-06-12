6e21fed8834600b6d07d0f2a49ee0d21d4f9e2fc
Lauren2544 <66128679+Lauren2544@users.noreply.github.com>
2020-06-12 12:40:44 -0400

    Create pointless.py

---
+++ pointless.py
@@ -0,0 +1,32 @@
+
+import os 
+import discord
+import datetime
+x = datetime.datetime.now()
+
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.content.find("$whatisthedate") != -1:
+        await message.channel.send(x)
+
+
+client.run(TOKEN)
+
+channel = discord.TextChannel()
+
+
+
+
+
+
