5b965d45f2752930619776ecd38b8e94fcf180b4
chriswkingg <christopherking@rogers.com>
2020-06-12 19:22:08 -0400

    Fixed joining and leaving, added error handling

--- message_handler.py
+++ message_handler.py
@@ -11,18 +11,22 @@ async def input_handler(message):
         await message.channel.send(datetime.datetime.now().strftime("%B " "%Y"))
     elif message_string.find("hi") != -1:
         await message.channel.send("Hi")
-    elif message.find("whatisthetime") != -1:
-        return datetime.datetime.now().strftime("The time is: %I:%M %p")
-    elif message.find("bye") != -1:
-        return "See you later"
+    elif message_string.find("whatisthetime") != -1:
+        await message.channel.send(datetime.datetime.now().strftime("The time is: %I:%M %p"))
     elif message_string.find("join") != -1:
-        await message.channel.send("Joining VC")
-        await voicechannel.connect()
+        try:
+            await message.channel.send("Joining VC")
+            await voicechannel.connect()
+        except discord.errors.ClientException:
+            await message.channel.send("Error: Already connected to a channel")
     elif message_string.find("leave") != -1:
         server=message.guild
         voice_client = server.voice_client
-        await message.channel.send("Leaving VC")
-        await voice_client.disconnect(force = True)
+        try: 
+            await message.channel.send("Leaving VC")
+            await voice_client.disconnect(force = True)
+        except AttributeError:
+            await message.channel.send("Error: Not connected to a voice channel")
     else:
-        response = ai.chatBotResponse(message)
-        return response if response else "Tammy doesn't know what you said! \n My devs are too dumb to teach me more things :("
+        response = ai.chatBotResponse(message_string)
+        await message.channel.send(response if response else "Tammy doesn't know what you said! \n My devs are too dumb to teach me more things :(")
