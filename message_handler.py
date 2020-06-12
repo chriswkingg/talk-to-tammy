9cde4aacd377bf66df61f5cc69914b3192179428
chriswkingg <christopherking@rogers.com>
2020-06-12 16:07:41 -0400

    Added nltk

--- message_handler.py
+++ message_handler.py
@@ -1,17 +1,11 @@
-
+import ai
 import datetime
 
 
 def input_handler(message):
-    if message.find("$whatisthedate") != -1:
+    if message.find("whatisthedate") != -1:
         return datetime.datetime.now().strftime("%B " "%Y")
-
-    elif message.find("$hi") != -1:
-        return "Hi"
-
-
-
-
-
-
-
+    else:
+        response = ai.chatBotResponse(message)
+        return response if response else "I just shat myself" //If the AI doesnt reply 
+    
