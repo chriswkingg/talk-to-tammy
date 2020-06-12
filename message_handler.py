5548c2f9b3482780b4e4b913698687fcecf6e3bd
Lauren2544 <66128679+Lauren2544@users.noreply.github.com>
2020-06-12 13:30:42 -0400

    Update message_handler.py

--- message_handler.py
+++ message_handler.py
@@ -4,10 +4,13 @@ import datetime
 
 def input_handler(message):
     if message.find("$whatisthedate") != -1:
-        return datetime.datetime.now().strftime("%B " "%Y")
+        return datetime.datetime.now().strftime("The date is: %A %B %Y")
 
     elif message.find("$hi") != -1:
         return "Hi"
+
+    elif message.find("$whatisthetime") != -1:
+        return datetime.datetime.now().strftime("The time is: %I:%M %p")
 
 
 
