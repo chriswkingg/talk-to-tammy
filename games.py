f317effd29226e46b2faa90e0d3df8d8b0bedac7
Lauren2544 <66128679+Lauren2544@users.noreply.github.com>
2020-06-12 20:13:33 -0400

    Update games.py

--- games.py
+++ games.py
@@ -9,9 +9,9 @@ async def playGame(message):
   name = 'Bob'
   await message.channel.send("Hello, " + name)
   await message.channel.send("1. Play word jumble")
-  print ("2. Play the monster game")
-  print ("3. Exit/Quit ")
-  print ("What would you like to do? ")
+  await ("2. Play the monster game")
+  await ("3. Exit/Quit ")
+  await ("What would you like to do? ")
 
 
   while True:
@@ -27,27 +27,27 @@ async def playGame(message):
               position = random.randrange(len(word))
               jumble += word[position]
               word = word[:position] + word[(position + 1):]
-          print ("")
-          print("Welcome to WORD JUMBLE!!! Unscramble the leters to make a word.")
-          print ("")
-          print("The jumble is:", jumble)
+          await ("")
+          await("Welcome to WORD JUMBLE!!! Unscramble the leters to make a word.")
+          await ("")
+          await("The jumble is:", jumble)
           guess = input("Your guess: ")
           while guess != correct and guess != "":
-              print ("")
-              print("Sorry, that's not it")
+              await ("")
+              await("Sorry, that's not it")
               guess = input("Your guess: ")
           if guess == correct:
-              print ("")
-              print("That's it, you guessed it!\n")
-          print("Thanks for playing")
-          print ("")
+              await ("")
+              await("That's it, you guessed it!\n")
+          await("Thanks for playing")
+          await ("")
         
     elif answer=="2": 
         from random import randint 
-        print ("Monster Game")
-        print ("There are three doors ahead... ")
-        print ("There is a monster behind one ")
-        print ("Which do you choose to open?")
+        await ("Monster Game")
+        await ("There are three doors ahead... ")
+        await ("There is a monster behind one ")
+        await("Which do you choose to open?")
         feeling_brave = True 
         score = 0
         while feeling_brave:
@@ -55,19 +55,19 @@ async def playGame(message):
           door = input("Door 1, 2 or 3:  ")
           door_num = int(door)
           if door_num <= 3 and door_num >0 and door_num == monster_door:
-            print ("Monster")
+            await ("Monster")
             feeling_brave = False 
           elif door_num <=3 and door_num >0: 
-            print ("No monster")
-            print ("You enter the next room.")
+            await ("No monster")
+            await ("You enter the next room.")
             score+=1
           else:
-            print ("Please enter a number between 1-3")
-        print ("Run away!")
-        print ("Game over, you scored", score)
-        print ("")
+            await ("Please enter a number between 1-3")
+        await ("Run away!")
+        await ("Game over, you scored", score)
+        await ("")
 
     elif answer=="3":
-      print ("That's a shame. Thanks for playing!\n") 
+      await ("That's a shame. Thanks for playing!\n") 
     elif answer=="":
-      print("Not Valid Choice Try again")
+      await("Not Valid Choice Try again")
