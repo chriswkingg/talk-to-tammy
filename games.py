aa3602db1c75c647f8a6e215ccca80cb50f5bc80
Lauren2544 <66128679+Lauren2544@users.noreply.github.com>
2020-06-12 20:50:39 -0400

    Update games.py

--- games.py
+++ games.py
@@ -40,29 +40,29 @@ async def playGame(message):
       await message.channel.send("")
         
     elif answer=="2": 
-        from random import randint 
-        await message.channel.send("Monster Game")
-        await message.channel.send("There are three doors ahead... ")
-        await message.channel.send("There is a monster behind one ")
-        await message.channel.send("Which do you choose to open?")
-        feeling_brave = True 
-        score = 0
-        while feeling_brave:
-          monster_door = randint (1,3)
-          door = input("Door 1, 2 or 3:  ")
-          door_num = int(door)
-          if door_num <= 3 and door_num >0 and door_num == monster_door:
-            await message.channel.send("Monster")
-            feeling_brave = False 
-          elif door_num <=3 and door_num >0: 
-            await message.channel.send("No monster")
-            await message.channel.send("You enter the next room.")
-            score+=1
-          else:
-            await message.channel.send("Please enter a number between 1-3")
-        await message.channel.send("Run away!")
-        await message.channel.send("Game over, you scored", score)
-        await message.channel.send("")
+      from random import randint 
+      await message.channel.send("Monster Game")
+      await message.channel.send("There are three doors ahead... ")
+      await message.channel.send("There is a monster behind one ")
+      await message.channel.send("Which do you choose to open?")
+      feeling_brave = True 
+      score = 0
+      while feeling_brave:
+        monster_door = randint (1,3)
+        door = input("Door 1, 2 or 3:  ")
+        door_num = int(door)
+        if door_num <= 3 and door_num >0 and door_num == monster_door:
+          await message.channel.send("Monster")
+          feeling_brave = False 
+        elif door_num <=3 and door_num >0: 
+          await message.channel.send("No monster")
+          await message.channel.send("You enter the next room.")
+          score+=1
+        else:
+          await message.channel.send("Please enter a number between 1-3")
+      await message.channel.send("Run away!")
+      await message.channel.send("Game over, you scored", score)
+      await message.channel.send("")
 
     elif answer=="3":
       await message.channel.send("That's a shame. Thanks for playing!\n") 
