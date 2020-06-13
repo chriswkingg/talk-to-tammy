6a57019c07ae6066ca3b967baaa20eda2ebfc965
Daniel <qiudaniel03@gmail.com>
2020-06-13 00:56:21 -0400

    Fixed formatting for ai

--- message_handler.py
+++ message_handler.py
@@ -13,17 +13,21 @@ async def input_handler(message):
     response = ""
     if message_string.find("whatisthedate") != -1:
         response = datetime.datetime.now().strftime("%B " "%Y")
+
     elif message_string.find("whatisthetime") != -1:
         response = datetime.datetime.now().strftime("The time is: %I:%M %p")
+
     elif message_string.find("google") != -1:
         quer = message_string.split(' ',2)[1]
         response = google_search.search(quer)
+
     elif message_string.find("join") != -1:
         try:
             response = "Joining VC"
             await voicechannel.connect()
         except discord.errors.ClientException:
             response = "Error: Already connected to a channel"
+
     elif message_string.find("leave") != -1:
         server=message.guild
         voice_client = server.voice_client
@@ -32,14 +36,17 @@ async def input_handler(message):
             await voice_client.disconnect(force = True)
         except AttributeError:
             response = "Error: Not connected to a voice channel"
+
     elif message_string.find("tts true") != -1:
         tts_enabled = True
         response = "tts enabled"
+
     elif message_string.find("tts false") != -1:
         tts_enabled = False
         response = "tts disabled"
+
     else:
         response = ai.chatBotResponse(message_string)
-        response = response if response else "Tammy doesn't know what you said! \nI shat myself"
+        response = response if response else "Tammy doesn't know what you said! \nMy devs are too dumb to teach me more things :("
     
     await message.channel.send(response, tts=tts_enabled)
