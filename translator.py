ee6b764766a95afa307ec94f2b7f9ff4a099cd21
Rohrim <rohanthemonkey@gmail.com>
2020-06-13 11:57:52 -0400

    Added more languages
    you can pick output language

--- translator.py
+++ translator.py
@@ -1,5 +1,8 @@
 from googletrans import Translator
 translator = Translator()
-def translate(message):
-    outmessage = translator.translate(message, dest='en')
+
+def translate(language, message):
+    Dict = dict ({'Arabic'	'ar', 'Bengali' : 'bn', 'Chinese' : 'zh', 'Croatian' : 'hr', 'Czech' : 'cs', 'Danish' : 'da', 'Dutch' : 'nl', 'English' : 'en', 'French' : 'fr','German' : 'de', 'Greek':'el', 'Hindi':'hi', 'Icelandic':'is', 'Indonesian':'id', 'Irish':'ga', 'Italian':'it', 'Japanese':'ja', 'Korean':'ko', 'Kurdish':'ku', 'Latin':'la', 'Latvian':'lv', 'Lithuanian':'lt', 'Mongolian':'mn', 'Persian':'fa', 'Polish':'pl', 'Portuguese':'pt', 'Punjabi':'pa', 'Romanian':'ro', 'Russian':'ru', 'Spanish':'es', 'Turkish':'tr', 'Ukrainian':'uk', 'Vietnamese':'vi', 'Zulu':'zu' })
+    dict.get(language)
+    outmessage = translator.translate(message, dest=language)
     return outmessage.text
