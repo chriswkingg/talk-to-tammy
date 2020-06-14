771b12af7078f110462efc9c26e7449a36e4b5f8
Daniel <qiudaniel03@gmail.com>
2020-06-14 07:31:01 -0400

    Limited wiki searches to 2000 char limit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # talk-to-tammy
 MLH Same home different hacks discord bot created by Chris King, Rohan Singh, Daniel Qiu, Lauren Rowe
-Created using Python and running on Google Cloud servers, Tammy is your own quarentine buddy. Not only is she able to keep a conversation,
+Created using Python and running on Google Cloud servers, Tammy is your own quarantine buddy. Not only is she able to keep a conversation,
 Tammy can be used for quick searches (weather, google, dictionary etc.)
--- wiki_search.py
+++ wiki_search.py
@@ -1,14 +1,17 @@
 import wikipedia
+import discord
 def findTitles(query):
     return wikipedia.search(query, results = 5)
 
 def articleSummary(query):
     try:
-        return wikipedia.summary(query)
+        return wikipedia.summary(query,chars=2000, auto_suggest=True, redirect=True)
     except wikipedia.exceptions.DisambiguationError:
         return "Be more specific!!!"
     except wikipedia.exceptions.PageError:
         return query + " does not match any pages"
+    except discord.errors.HTTPException:
+        return "The summary cant be loaded, check out the link: " + getLink(query)
 
 def getLink(query):
     try:
