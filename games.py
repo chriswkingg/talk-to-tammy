08f9bf3c701bf4bdf87144fcbc000fe5b69fc796
Daniel <qiudaniel03@gmail.com>
2020-06-14 00:18:25 -0400

    Added a new game!

--- games.py
+++ games.py
@@ -2,6 +2,7 @@ import message_handler
 import random
 import asyncio
 import restart_app
+from random import randint
 
 game_enabled = False
 
@@ -18,7 +19,7 @@ async def init_games(client, message):
         if message2.content.lower() == "1":
             await game_1(client, message)
         elif message2.content.lower() == "2":
-            await message2.channel.send("Chris has big pp")
+            await game_2(client, message)
 
 async def game_1(client, message):
     global game_enabled
@@ -61,3 +62,47 @@ async def game_1(client, message):
             return
 
     return
+
+score = 0
+async def game_2(client, message):
+    global score
+    global game_enabled
+    game_enabled = True
+    await message.channel.send("Monster Game")
+    await message.channel.send("There are three doors ahead... ")
+    await message.channel.send("There is a monster behind one ")
+    await message.channel.send("Which do you choose to open?")
+
+    
+
+    await message.channel.send("Door 1, 2 or 3:  ")
+
+    global score
+
+    @client.event
+    async def on_message(message2):
+        global score
+        guess = message2.content.lower()
+        if message2.author == client.user:
+            return
+        monster_door = randint (1,3)
+        door_num = int(guess)
+        if door_num <= 3 and door_num >0 and door_num == monster_door:
+            await message.channel.send("Monster")
+            await message.channel.send("Run away!")
+            await message.channel.send("Game over, you scored: " + str(score))
+            restart_app.restart_program()
+        elif door_num <=3 and door_num >0: 
+            await message.channel.send("No monster")
+            await message.channel.send("You enter the next room.")
+            score+=1
+            await message.channel.send("Please enter a number between 1-3")
+
+
+
+
+
+#   elif answer=="3":
+#     await message.channel.send("That's a shame. Thanks for playing!\n") 
+#   elif answer=="":
+#     await message.channel.send("Not Valid Choice Try again")
